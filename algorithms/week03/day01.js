a1db2404340299e7cb29d76e660d51f56f6a6d50
Grendel <jcg@grendel.world>
2023-10-16 10:38:08 -0500

    algos: Complete week 3 day 1

--- algorithms/week03/day01.js
+++ algorithms/week03/day01.js
@@ -22,14 +22,25 @@ const expected4a = 3;
 
 /**
  * Returns the index of the smallest number from the given array.
- * - Time: O(?).
- * - Space: O(?).
+ * - Time: O(n).
+ * - Space: O(1).
  * @param {Array<number>} nums
  * @returns {number} Index of smallest number or -1 if empty given array.
  */
 function indexOfMinVal(nums) {
-    // code here
+	let smollestIndex = -1;
+	for (let i=0; i<nums.length; i++) {
+		if (smollestIndex < 0 || nums[i] < nums[smollestIndex]) {
+			smollestIndex = i;
+		}
+	}
+	return smollestIndex;
 }
+
+console.log(indexOfMinVal(nums1), "should be", expected1a);
+console.log(indexOfMinVal(nums2), "should be", expected2a);
+console.log(indexOfMinVal(nums3), "should be", expected3a);
+console.log(indexOfMinVal(nums4), "should be", expected4a);
 
 /*******************************************************************************/
 
@@ -52,14 +63,28 @@ const expected3 = ["a"];
 const arr4 = [];
 const expected4 = [];
 
+const arr5 = ["c","h","u","n","g","u","s","a","m","o","g","u","s"];
+const expected5 = ["s","u","g","o","m","a","s","u","g","n","u","h","c"];
+
 /**
  * Reverses the given arr in place without built in methods
  * BONUS: Do this in place without built ins
- * - Time: O(?).
- * - Space: O(?).
+ * - Time: O(n).
+ * - Space: O(1).
  * @param {Array<any>} items
  * @returns {Array<any>} The given arr after being reversed.
  */
 function reverseArr(items) {
-    // code here
+	for (let i=0, j=items.length-1; i<j; i++, j--) {
+		let k = items[i];
+		items[i] = items[j];
+		items[j] = k;
+	}
+	return items;
 }
+
+console.log(reverseArr(arr1), "should be", expected1);
+console.log(reverseArr(arr2), "should be", expected2);
+console.log(reverseArr(arr3), "should be", expected3);
+console.log(reverseArr(arr4), "should be", expected4);
+console.log(reverseArr(arr5), "should be", expected5);
