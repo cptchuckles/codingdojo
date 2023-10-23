320a51f5eac57188e5143265f9f5cb4346b6f863
Grendel <jcg@grendel.world>
2023-10-23 13:24:01 -0500

    python: Add some introduction files

---
+++ python/introduction/hello-world/hello-world.py
@@ -0,0 +1,36 @@
+# step 1
+print("henlo worl")
+
+
+# step 2
+name = "grendel"
+
+print("henlo", name, "!")
+
+
+# step 3
+print("henlo " + name + "!")
+
+
+# step 4
+favorite_number = 62
+print("henlo", favorite_number, "!")
+
+
+# step 5
+print("henlo " + str(favorite_number) + "!")
+
+
+# step 6
+first_favorite_food = "chicken"
+second_favorite_food = "crab"
+
+print("my favorite foods are {} and {}".format(first_favorite_food, second_favorite_food))
+
+
+# step 7
+print(f"my favorite foods are {first_favorite_food} and {second_favorite_food}")
+
+
+# step 8
+print("my favorite foods are %s and %s" % (first_favorite_food, second_favorite_food))
