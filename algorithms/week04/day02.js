8522b37bec57820e7953ef18cbc47d75c5ebea73
Grendel <jcg@grendel.world>
2023-10-24 10:36:12 -0500

    algos: add second part for week 4 day 2

--- algorithms/week04/day02.js
+++ algorithms/week04/day02.js
@@ -1,6 +1,7 @@
-/* 
+{
+    /* 
   Given an arr and a separator string, output a string of every item in the array separated by the separator.
-  
+
   No trailing separator at the end
   Default the separator to a comma with a space after it if no separator is provided
 */
@@ -8,27 +9,27 @@
 
 
 
-const arr1 = [1, 2, 3];
-const separator1 = ", ";
-const expected1 = "1, 2, 3";
+    const arr1 = [1, 2, 3];
+    const separator1 = ", ";
+    const expected1 = "1, 2, 3";
 
-const arr2 = [1, 2, 3];
-const separator2 = "-";
-const expected2 = "1-2-3";
+    const arr2 = [1, 2, 3];
+    const separator2 = "-";
+    const expected2 = "1-2-3";
 
-const arr3 = [1, 2, 3];
-const separator3 = " - ";
-const expected3 = "1 - 2 - 3";
+    const arr3 = [1, 2, 3];
+    const separator3 = " - ";
+    const expected3 = "1 - 2 - 3";
 
-const arr4 = [1];
-const separator4 = ", ";
-const expected4 = "1";
+    const arr4 = [1];
+    const separator4 = ", ";
+    const expected4 = "1";
 
-const arr5 = [];
-const separator5 = ", ";
-const expected5 = "";
+    const arr5 = [];
+    const separator5 = ", ";
+    const expected5 = "";
 
-/**
+    /**
  * Converts the given array into a string of items separated by the given separator.
  * - Time: O(?).
  * - Space: O(?).
@@ -36,21 +37,72 @@ const expected5 = "";
  * @param {string} separator To separate each item of the given arr.
  * @returns {string} The given array items as a string separated by the given separator.
  */
-function join(arr, separator) {
-    let result = "";
-    for (let i in arr) {
-        result += String(arr[i]);
-        if (i < arr.length-1) {
-            result += separator;
+    function join(arr, separator) {
+        let result = "";
+        for (let i in arr) {
+            result += String(arr[i]);
+            if (i < arr.length-1) {
+                result += separator;
+            }
         }
+        return result;
     }
-    return result;
+
+    console.log(join(arr1,separator1), "should be", expected1);
+    console.log(join(arr2,separator2), "should be", expected2);
+    console.log(join(arr3,separator3), "should be", expected3);
+    console.log(join(arr4,separator4), "should be", expected4);
+    console.log(join(arr5,separator5), "should be", expected5);
+
+
+}/*****************************************************************************/
+
+
+{
+    /* 
+  Acronyms
+
+  Create a function that, given a string, returns the string’s acronym 
+  (first letter of each word capitalized). 
+
+  Do it with .split first if you need to, then try to do it without
+*/
+
+    const str1 = "object oriented programming";
+    const expected1 = "OOP";
+
+    // The 4 pillars of OOP
+    const str2 = "abstraction polymorphism inheritance encapsulation";
+    const expected2 = "APIE";
+
+    const str3 = "software development life cycle";
+    const expected3 = "SDLC";
+
+    // Bonus: ignore extra spaces
+    const str4 = "  global   information tracker    ";
+    const expected4 = "GIT";
+
+    /**
+ * Turns the given str into an acronym.
+ * - Time: O(?).
+ * - Space: O(?).
+ * @param {string} wordsStr A string to be turned into an acronym.
+ * @returns {string} The acronym.
+ */
+    function acronymize(str) {
+        let result = "";
+        // see: regex101.com
+        const regex = /\b\w+\b/g;
+        for (let word of str.match(regex)) {
+            result += word[0].toUpperCase();
+        }
+        return result;
+    }
+
+    console.log(acronymize(str1), "should be", expected1);
+    console.log(acronymize(str2), "should be", expected2);
+    console.log(acronymize(str3), "should be", expected3);
+    console.log(acronymize(str4), "should be", expected4);
+
+    /*****************************************************************************/
 }
-
-console.log(join(arr1,separator1), "should be", expected1);
-console.log(join(arr2,separator2), "should be", expected2);
-console.log(join(arr3,separator3), "should be", expected3);
-console.log(join(arr4,separator4), "should be", expected4);
-console.log(join(arr5,separator5), "should be", expected5);
-
-/*****************************************************************************/
