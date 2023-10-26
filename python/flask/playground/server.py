d6ba5834adb5e91c07bf41b8a0222b27cb42e93a
Grendel <jcg@grendel.world>
2023-10-26 16:04:20 -0500

    python(flask): complete playground

--- python/flask/playground/server.py
+++ python/flask/playground/server.py
@@ -8,9 +8,11 @@ def index():
     return "go to the playground idiot"
 
 
-@app.route("/play/<int:count>")
-def play(count: int):
-    return render_template("index.html", count=count)
+@app.route("/play", defaults={"count": 3, "color": "lightblue"})
+@app.route("/play/<int:count>", defaults={"color": "lightblue"})
+@app.route("/play/<int:count>/<color>")
+def play(count: int, color: str):
+    return render_template("index.html", count=count, color=color)
 
 
 if __name__ == "__main__":
