42d8c37048cb61e653d5e2ea833a244a800ef216
Grendel <jcg@grendel.world>
2023-10-29 19:02:19 -0500

    python(flask): complete number game MVP

--- python/flask/number/server.py
+++ python/flask/number/server.py
@@ -1,10 +1,34 @@
-from flask import Flask, render_template, redirect, session, request
+from flask import Flask, render_template, session, redirect, request
+import random
+
+
 app = Flask(__name__)
+app.secret_key = "no peeking"
 
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    if "number" not in session:
+        session["number"] = random.randrange(1, 100)
+    if "guess" not in session:
+        session["guess"] = -1
+    if "tries" not in session:
+        session["tries"] = 1
+    else:
+        session["tries"] += 1
+    return render_template("index.html", tries=session["tries"], guess=session["guess"], number=session["number"])
+
+
+@app.route("/reset")
+def reset():
+    session.clear()
+    return redirect("/")
+
+
+@app.route("/guess", methods=["POST"])
+def guess():
+    session["guess"] = int(request.form["guess"])
+    return redirect("/")
 
 
 if __name__ == "__main__":
