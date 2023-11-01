cf838fc1bd0eb70157ebfb76390d081b2c96c06f
Grendel <jcg@grendel.world>
2023-11-01 17:16:59 -0500

    mysql(first_flask): simplify Friend#get_all

--- mysql/first_flask/friend.py
+++ mysql/first_flask/friend.py
@@ -19,11 +19,7 @@ class Friend:
         # make sure to call the connectToMySQL function with the schema you are targeting.
         results = connectToMySQL('first_flask').query_db(query)
         # Create an empty list to append our instances of friends
-        friends = []
-        # Iterate over the db results and create instances of friends with cls.
-        for friend in results:
-            friends.append(cls(friend))
-        return friends
+        return [cls(result) for result in results]
 
     @classmethod
     def save(cls, data):
