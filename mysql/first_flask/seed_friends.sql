54bbc0c06d9b10fc4454ecb95f7e56e88fa1eca3
Grendel <jcg@grendel.world>
2023-11-01 12:54:46 -0500

    mysql(first_flask): add base db files

---
+++ mysql/first_flask/seed_friends.sql
@@ -0,0 +1,5 @@
+INSERT INTO friends (first_name, last_name, occupation) VALUES
+    ("Johnny", "Five", "Killer robot"),
+    ("John", "Connor", "Robot killer"),
+    ("John", "Sekiro", "Killer"),
+    ("Jonathan", "Gollnick", NULL);
