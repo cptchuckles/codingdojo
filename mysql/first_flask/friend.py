0d8becf611bbe6d00b2b3d70b271adb715fe1985
Grendel <jcg@grendel.world>
2023-11-01 12:57:50 -0500

    mysql(first_flask): fix formatting

--- mysql/first_flask/friend.py
+++ mysql/first_flask/friend.py
@@ -1,8 +1,10 @@
 # import the function that will return an instance of a connection
 from mysqlconnection import connectToMySQL
 # model the class after the friend table from our database
+
+
 class Friend:
-    def __init__( self , data ):
+    def __init__(self, data):
         self.id = data['id']
         self.first_name = data['first_name']
         self.last_name = data['last_name']
@@ -10,6 +12,7 @@ class Friend:
         self.created_at = data['created_at']
         self.updated_at = data['updated_at']
     # Now we use class methods to query our database
+
     @classmethod
     def get_all(cls):
         query = "SELECT * FROM friends;"
@@ -19,6 +22,5 @@ class Friend:
         friends = []
         # Iterate over the db results and create instances of friends with cls.
         for friend in results:
-            friends.append( cls(friend) )
+            friends.append(cls(friend))
         return friends
-            
