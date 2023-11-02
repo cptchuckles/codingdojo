9f09f8b1c53d802c9c8a6a8e341bc226bdfbe6f0
Grendel <jcg@grendel.world>
2023-11-01 21:49:52 -0500

    mysql(first_flask): complete CRUD server endpoints

--- python/mysql/first_flask/friend.py
+++ python/mysql/first_flask/friend.py
@@ -11,14 +11,21 @@ class Friend:
         self.occupation = data['occupation']
         self.created_at = data['created_at']
         self.updated_at = data['updated_at']
-    # Now we use class methods to query our database
+
+    @classmethod
+    def get(cls, id: int):
+        query = "SELECT * FROM friends WHERE id = %(id)s"
+
+        result = connectToMySQL('first_flask').query_db(query, {"id": id})
+
+        return cls(result[0]) if result else None
 
     @classmethod
     def get_all(cls):
         query = "SELECT * FROM friends;"
-        # make sure to call the connectToMySQL function with the schema you are targeting.
+
         results = connectToMySQL('first_flask').query_db(query)
-        # Create an empty list to append our instances of friends
+
         return [cls(result) for result in results]
 
     @classmethod
@@ -34,4 +41,31 @@ class Friend:
             "occupation": occupation,
         }
 
-        return connectToMySQL('first_flask').query_db(query, data)
+        connectToMySQL('first_flask').query_db(query, data)
+
+        query = """
+            SELECT * FROM friends ORDER BY id DESC LIMIT 1
+        """
+        new_friend_data = connectToMySQL('first_flask').query_db(query)[0]
+
+        return cls(new_friend_data)
+
+    @classmethod
+    def delete(cls, id: int):
+        query = "DELETE FROM friends WHERE id = %(id)s"
+
+        return connectToMySQL('first_flask').query_db(query, {"id": id})
+
+    @classmethod
+    def update(cls, id: int, new_data):
+        query = """
+            UPDATE friends SET
+            first_name = %(first_name)s,
+            last_name = %(last_name)s,
+            occupation = %(occupation)s
+            WHERE id = %(id)s
+        """
+
+        new_data.id = id
+
+        return connectToMySQL('first_flask').query_db(query, new_data)
--- python/mysql/first_flask/server.py
+++ python/mysql/first_flask/server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, abort
 from friend import Friend
 
 app = Flask(__name__)
@@ -10,15 +10,47 @@ def index():
     return render_template("index.html", friends=friends)
 
 
+@app.route("/<int:id>")
+def view(id: int):
+    friend = Friend.get(id)
+
+    if not friend:
+        return abort(404)
+    else:
+        return render_template("view.html", friend=friend)
+
+
 @app.route("/new")
 def new_fren():
     return render_template("new.html")
 
 
+@app.route("/edit/<int:id>")
+def edit_fren(id: int):
+    friend = Friend.get(id)
+
+    if not friend:
+        return "nigga that shit ain't even real", 404
+    else:
+        return render_template("edit.html", friend=friend)
+
+
+@app.route("/delete/<int:id>")
+def delet_fren(id: int):
+    Friend.delete(id)
+    return redirect("/")
+
+
 @app.route("/create", methods=["POST"])
 def create_fren():
     new_friend = Friend.create(**request.form)
-    return redirect("/")
+    return redirect(f"/{new_friend.id}")
+
+
+@app.route("/update", methods=["POST"])
+def update_fren():
+    Friend.update(int(request.form["id"]), request.form)
+    return redirect(f"/{request.form['id']}")
 
 
 if __name__ == "__main__":
