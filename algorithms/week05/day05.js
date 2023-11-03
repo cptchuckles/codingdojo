8bec739601de6b1d5be4e7fa33cc378ef508c8eb
Grendel <jcg@grendel.world>
2023-11-03 10:28:31 -0500

    algos: solve week 5 day 5

--- algorithms/week05/day05.js
+++ algorithms/week05/day05.js
@@ -8,6 +8,9 @@
     const str1 = "   hello world     ";
     const expected1 = "hello world";
 
+    const str2 = "             ";
+    const expected2 = "";
+
     /**
  * Trims any leading or trailing white space from the given str.
  * - Time: O(?).
@@ -16,9 +19,25 @@
  * @returns {string} The given string with any leading or trailing white space
  *    stripped.
  */
-    function trim(str) {}
+    function trim(str) {
+        let start = 0;
+        let end = str.length-1;
+        while (true) {
+            if (str[start] !== " " && str[end] !== " ") {
+                break;
+            }
+            if (str[start] === " ") {
+                start++;
+            }
+            if (str[end] === " ") {
+                end--;
+            }
+        }
+        return str.slice(start, end+1);
+    }
 
     console.log(trim(str1), "should be", expected1);
+    console.log(trim(str2), "should be", expected2);
     /*****************************************************************************/
 }
 
@@ -49,6 +68,10 @@
     const strB4 = "listen";
     const expected4 = true;
 
+    const strA5 = "hello";
+    const strB5 = "lelho";
+    const expected5 = true;
+
     /**
  * Determines whether s1 and s2 are anagrams of each other.
  * Anagrams have all the same letters but in different orders.
@@ -58,11 +81,48 @@
  * @param {string} s2
  * @returns {boolean} Whether s1 and s2 are anagrams.
  */
-    function isAnagram(s1, s2) {}
+    function isAnagram(s1, s2) {
+        if (s1.length !== s2.length) {
+            return false;
+        }
+
+        const freqs = {};
+        for (let char of s1.toLowerCase()) {
+            freqs[char] = (freqs[char] || 0) + 1;
+        }
+
+        for (let char of s2.toLowerCase()) {
+            if (! char in freqs) {
+                return false;
+            }
+            freqs[char] -= 1;
+            if (freqs[char] < 0) {
+                return false;
+            }
+        }
+
+        for (const val of Object.values(freqs)) {
+            if (val !== 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    function isAnagramEZ(s1, s2) {
+        return s1.toLowerCase().split('').sort().join('') === s2.toLowerCase().split('').sort().join('');
+    }
 
     console.log(isAnagram(strA1, strB1), "should be", expected1);
     console.log(isAnagram(strA2, strB2), "should be", expected2);
     console.log(isAnagram(strA3, strB3), "should be", expected3);
     console.log(isAnagram(strA4, strB4), "should be", expected4);
+    console.log(isAnagram(strA5, strB5), "should be", expected5);
+    console.log(isAnagramEZ(strA1, strB1), "(EZ) should be", expected1);
+    console.log(isAnagramEZ(strA2, strB2), "(EZ) should be", expected2);
+    console.log(isAnagramEZ(strA3, strB3), "(EZ) should be", expected3);
+    console.log(isAnagramEZ(strA4, strB4), "(EZ) should be", expected4);
+    console.log(isAnagramEZ(strA5, strB5), "(EZ) should be", expected5);
     /*****************************************************************************/
 }
