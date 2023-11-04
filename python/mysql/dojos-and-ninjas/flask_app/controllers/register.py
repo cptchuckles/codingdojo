16312c9dc51389b231233056d285d495f0d8ad99
Grendel <jcg@grendel.world>
2023-11-04 10:33:21 -0500

    mysql(dojos): add one-to-many route registration

--- python/mysql/dojos-and-ninjas/flask_app/controllers/register.py
+++ python/mysql/dojos-and-ninjas/flask_app/controllers/register.py
@@ -51,3 +51,17 @@ def register_crud_routes_for(model: ModelBase):
     def delete(id: int):
         model.delete(id)
         return redirect(f"/{model_name}")
+
+
+def register_one_to_many_routes(one: ModelBase, many: ModelBase):
+    one_name = one.__name__.lower()
+    many_name = many.__name__.lower()
+    many_collection = many_name + "s"
+
+    @app.route(f"/{one_name}/<int:id>/{many_name}", endpoint=f"{one_name}/has/{many_name}s")
+    def one_to_many(id: int):
+        one_has_many = one.one_join_many(id, many)
+        if one_has_many is None:
+            return abort(404)
+        else:
+            return render_template(f"/views/{many_name}/index.html", **{many_collection: one_has_many[1]})
