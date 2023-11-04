47c2f95d4bf5c85107e4808b4e2adbbbd9d8c481
Grendel <jcg@grendel.world>
2023-11-04 11:49:47 -0500

    mysql(dojos): create view for dojo and ninja, and ninjas of a dojo

--- python/mysql/dojos-and-ninjas/flask_app/controllers/__init__.py
+++ python/mysql/dojos-and-ninjas/flask_app/controllers/__init__.py
@@ -1,5 +1,6 @@
-from flask_app.controllers.register import register_crud_routes_for
+from flask_app.controllers.register import register_crud_routes_for, register_one_to_many_routes
 import flask_app.models as models
 
 register_crud_routes_for(models.Dojo)
 register_crud_routes_for(models.Ninja)
+register_one_to_many_routes(models.Dojo, models.Ninja)
