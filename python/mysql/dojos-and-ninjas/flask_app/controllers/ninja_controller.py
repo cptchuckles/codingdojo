c1c24ff66672e16d752d260133e6011305b974bd
Grendel <jcg@grendel.world>
2023-11-04 16:30:54 -0500

    mysql(dojos): add new ninja form

--- python/mysql/dojos-and-ninjas/flask_app/controllers/ninja_controller.py
+++ python/mysql/dojos-and-ninjas/flask_app/controllers/ninja_controller.py
@@ -1,6 +1,7 @@
-from flask import render_template, abort
+from flask import render_template, abort, request
 from flask_app.controllers.controller_base import ControllerBase
 from flask_app.models import Ninja, Dojo
+from flask_app import app
 
 
 class NinjaController(ControllerBase):
@@ -17,3 +18,9 @@ class NinjaController(ControllerBase):
         dojo = ninja_with_dojo[1]
 
         return render_template("views/ninja/view.html", ninja=ninja, dojo=dojo)
+
+    def new(self):
+        dojo_id = request.args.get("dojo_id")
+        if dojo_id is not None:
+            dojo_id = int(dojo_id)
+        return render_template("views/ninja/new.html", dojo_id=dojo_id, dojos=Dojo.get_all())
