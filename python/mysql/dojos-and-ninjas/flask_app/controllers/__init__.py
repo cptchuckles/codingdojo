601116f7bc50cbf0e15c6ffb590e0bf1ef1000ec
Grendel <jcg@grendel.world>
2023-11-04 12:11:02 -0500

    mysql(dojos): make controllers OOP again

--- python/mysql/dojos-and-ninjas/flask_app/controllers/__init__.py
+++ python/mysql/dojos-and-ninjas/flask_app/controllers/__init__.py
@@ -1,6 +1,8 @@
-from flask_app.controllers.register import register_crud_routes_for, register_one_to_many_routes
+from flask_app.controllers.register import register_one_to_many_routes
+from flask_app.controllers.controller_base import ControllerBase
 import flask_app.models as models
 
-register_crud_routes_for(models.Dojo)
-register_crud_routes_for(models.Ninja)
+ControllerBase(models.Dojo).register_crud_routes()
+ControllerBase(models.Ninja).register_crud_routes()
+
 register_one_to_many_routes(models.Dojo, models.Ninja)
--- python/mysql/dojos-and-ninjas/flask_app/controllers/register.py
+++ python/mysql/dojos-and-ninjas/flask_app/controllers/register.py
@@ -1,56 +1,6 @@
-from flask import render_template, abort, request, redirect
+from flask import render_template, abort
 from flask_app.models import ModelBase
 from flask_app import app
-
-
-def register_crud_routes_for(model: ModelBase):
-    model_name = model.__name__.lower()
-    item_name = model_name
-    collection_name = model_name + "s"
-
-    @app.route(f"/{model_name}", endpoint=f"{model_name}/index")
-    def index():
-        collection = model.get_all()
-        return render_template(f"views/{model_name}/index.html", **{collection_name: collection})
-
-    @app.route(f"/{model_name}/<int:id>", endpoint=f"{model_name}/view")
-    def view(id: int):
-        item = model.get_by_id(id)
-
-        if item is None:
-            return abort(404)
-        else:
-            return render_template(f"/views/{model_name}/view.html", **{item_name: item})
-
-    @app.route(f"/{model_name}/new", endpoint=f"{model_name}_new")
-    def new():
-        return render_template(f"/views/{model_name}/new.html")
-
-    @app.route(f"/{model_name}/create", methods=["POST"], endpoint=f"{model_name}/create")
-    def create():
-        new_id = model.create(request.form)
-        return redirect(f"/{model_name}/{new_id}")
-
-    @app.route(f"/{model_name}/<int:id>/edit", endpoint=f"{model_name}/edit")
-    def edit(id: int):
-        item = model.get_by_id(id)
-        if item is None:
-            return abort(404)
-        else:
-            return render_template(f"/views/{model_name}/edit.html", **{item_name: item})
-
-    @app.route(f"/{model_name}/update", methods=["POST"], endpoint=f"{model_name}/update")
-    def update():
-        item = model.update(request.form)
-        if item is None:
-            return abort(500)
-        else:
-            return redirect(f"/views/{model_name}/{item.id}")
-
-    @app.route(f"/{model_name}/<int:id>/delete", endpoint=f"{model_name}/delete")
-    def delete(id: int):
-        model.delete(id)
-        return redirect(f"/{model_name}")
 
 
 def register_one_to_many_routes(one: ModelBase, many: ModelBase):
