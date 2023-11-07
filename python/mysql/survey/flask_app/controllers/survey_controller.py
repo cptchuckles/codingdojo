6b1604f5c2911559e78a280a4262d5a2212c2c99
Grendel <jcg@grendel.world>
2023-11-06 20:06:58 -0600

    mysql(survey): add validation and flash warnings in place

--- python/mysql/survey/flask_app/controllers/survey_controller.py
+++ python/mysql/survey/flask_app/controllers/survey_controller.py
@@ -1,3 +1,4 @@
+from flask import redirect
 from flask_app.controllers.controller_base import ControllerBase
 from flask_app.models.survey import Survey
 
@@ -5,3 +6,9 @@ from flask_app.models.survey import Survey
 class SurveyController(ControllerBase):
     def __init__(self):
         super().__init__(Survey)
+
+    def create(self, form_data):
+        if not Survey.validate(form_data):
+            return redirect("/survey/new")
+        else:
+            return super().create(form_data)
--- python/mysql/survey/flask_app/models/survey.py
+++ python/mysql/survey/flask_app/models/survey.py
@@ -1,3 +1,4 @@
+from flask import flash
 from flask_app.models.model_base import ModelBase
 
 
@@ -23,3 +24,24 @@ class Survey(ModelBase):
         new_data["tablet"] = "tablet" in data
         new_data["phone"] = "phone" in data
         return super().create(new_data)
+
+    @staticmethod
+    def validate(data):
+        valid = True
+        if len(data["name"]) < 2:
+            flash("Name must be longer than 1 character.", "name")
+            valid = False
+        if data["location"] == "Chernobyl":
+            flash("Please do not submit surveys from Chernobyl.", "location")
+            valid = False
+        if data["favorite_language"] == "Fish":
+            flash("Why do you have to be a bad person?", "language")
+            valid = False
+        if data["paradigm"] == "Declarative":
+            flash("Please choose a real programming paradigm.", "paradigm")
+            valid = False
+        if "phone" not in data:
+            flash("Do you guys not have phones??", "devices")
+            valid = False
+
+        return valid
