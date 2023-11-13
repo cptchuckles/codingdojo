ef676567bbae5f5086303fe53459514eef83f4cc
Grendel <jcg@grendel.world>
2023-11-13 10:39:59 -0600

    algos: add week 7 day 1 challenge

---
+++ algorithms/week07/day01.js
@@ -0,0 +1,27 @@
+/* 
+  Recursively sum an arr of ints
+*/
+
+const nums1 = [1, 2, 3];
+const expected1 = 6;
+
+const nums2 = [1];
+const expected2 = 1;
+
+const nums3 = [];
+const expected3 = 0;
+
+const nums4 = [1, 2, 3, 4, 5];
+const expected4 = 15;
+
+/**
+ * Add params if needed for recursion
+ * Recursively sums the given array.
+ * - Time: O(?).
+ * - Space: O(?).
+ * @param {Array<number>} nums
+ * @returns {number} The sum of the given nums.
+ */
+function sumArr(nums) {}
+
+/*****************************************************************************/
