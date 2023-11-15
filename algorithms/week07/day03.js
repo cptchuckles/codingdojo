99b5b7cb844c38af2fff3ccfd5c05955eb4d49d9
Grendel <jcg@grendel.world>
2023-11-15 10:22:33 -0600

    algos: add week 7 day 3 solution

---
+++ algorithms/week07/day03.js
@@ -0,0 +1,36 @@
+/* 
+  Recursively reverse a string
+  helpful methods:
+
+  str.slice(beginIndex[, endIndex])
+  returns a new string from beginIndex to endIndex exclusive
+*/
+
+const str1 = "abc";
+const expected1 = "cba";
+
+const str2 = "";
+const expected2 = "";
+
+const str3 = "typewriter";
+const expected3 = "retirwepyt";
+
+/**
+ * Recursively reverses a string.
+ * - Time: O(?).
+ * - Space: O(?).
+ * @param {string} str
+ * @returns {string} The given str reversed.
+ */
+function reverseStr(str) {
+    if (str === "") {
+        return str;
+    }
+    return str.slice(str.length-1) + reverseStr(str.slice(0, str.length-1));
+}
+
+console.log(reverseStr(str1), "should be", expected1);
+console.log(reverseStr(str2), "should be", expected2);
+console.log(reverseStr(str3), "should be", expected3);
+
+/*****************************************************************************/
