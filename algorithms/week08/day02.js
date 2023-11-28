4ac3185a3e8cfceec7db2f6c850879182cd54214
Grendel <jcg@grendel.world>
2023-11-28 10:17:58 -0600

    algos(w8d2): add problem

---
+++ algorithms/week08/day02.js
@@ -0,0 +1,23 @@
+// MERN Algos Week 1 - Sorting Algorithms
+// https://www.bigocheatsheet.com/
+
+// Bubble Sort
+
+// For every pair of adjacent indicies
+// swap them if the item on the left is larger than the item on the right
+// continue until array is fully sorted
+
+function bubbleSort(arr) {
+    // code
+    return arr;
+}
+
+// console.log(bubbleSort([78, 5, 678, 765, 456, 78, 265, 908]))
+
+
+// Time Complexity
+// - BEST: n when array is sorted
+// - AVERAGE: O(n^2)
+// - WORST: O(n^2)
+
+// https://www.hackerearth.com/practice/algorithms/sorting/bubble-sort/visualize/
