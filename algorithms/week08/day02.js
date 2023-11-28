acec4647480f21ad80a507e4541bb7c03cdaad26
Grendel <jcg@grendel.world>
2023-11-28 10:38:20 -0600

    algos(w8d2): improve readability

--- algorithms/week08/day02.js
+++ algorithms/week08/day02.js
@@ -13,18 +13,22 @@
 function bubbleSort(arr) {
     for (let end=arr.length-1; end>0; end--) {
         let sorted = true;
+
         for (let i=0; i<end; i++) {
             if (arr[i] > arr[i+1]) {
-                sorted = false;
                 const tmp = arr[i];
                 arr[i] = arr[i+1];
                 arr[i+1] = tmp;
+
+                sorted = false;
             }
         }
-        if (sorted === true) {
+
+        if (sorted) {
             break;
         }
     }
+
     return arr;
 }
 
