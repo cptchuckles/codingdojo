747acfe797132b7e92b40b56bfad49e46be1be4b
Grendel <jcg@grendel.world>
2023-11-30 14:22:38 -0600

    mern: complete hook form assignment

---
+++ mern/hook-form/src/App.jsx
@@ -0,0 +1,81 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+
+function App() {
+  const defaultFormData = {
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  };
+
+  const [formData, setFormData] = useState({...defaultFormData});
+
+  /**
+   * Submits the form and clears user input
+   *
+   * @param {SubmitEvent} ev
+   */
+  const submitForm = (ev) => {
+    ev.preventDefault();
+
+    console.log(formData);
+
+    setFormData({...defaultFormData});
+    ev.target.reset();
+  };
+
+  return (
+    <>
+      <h1>MY MAGICAL FORM</h1>
+      <form onSubmit={submitForm}>
+        <label htmlFor="firstName">
+          First Name:
+          <input onChange={(e) => setFormData({...formData, firstName: e.target.value})} type="text" name="firstName" id="firstName" />
+        </label>
+        <label htmlFor="lastName">
+          Last Name:
+          <input onChange={(e) => setFormData({...formData, lastName: e.target.value})} type="text" name="lastName" id="lastName" />
+        </label>
+        <label htmlFor="email">
+          Email:
+          <input onChange={(e) => setFormData({...formData, email: e.target.value})} type="email" name="email" id="email" />
+        </label>
+        <label htmlFor="password">
+          Password:
+          <input onChange={(e) => setFormData({...formData, password: e.target.value})} type="password" name="password" id="password" />
+        </label>
+        <label htmlFor="confirmPassword">
+          Confirm Password:
+          <input onChange={(e) => setFormData({...formData, confirmPassword: e.target.value})} type="password" name="confirmPassword" id="confirmPassword" />
+        </label>
+        <button>SuBmIT</button>
+      </form>
+      <h2>Your Form Data:</h2>
+      <table>
+        <thead>
+          <tr>
+            <th>Field</th>
+            <th>Value</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            Object.entries({...formData}).map(([k,v]) => (
+              <tr key={k}>
+                <td>{k}</td>
+                <td>{v}</td>
+              </tr>
+            ))
+          }
+        </tbody>
+      </table>
+    </>
+  )
+}
+
+export default App
