3ee3f70d8c41650fa6fe24475e363e73f2354afb
Grendel <jcg@grendel.world>
2023-12-01 15:52:05 -0600

    mern(boxGenerator): fix whitespace

--- mern/box-generator/src/BoxForm.jsx
+++ mern/box-generator/src/BoxForm.jsx
@@ -1,16 +1,16 @@
 
 const BoxForm = ({submitHandler}) => {
-	return (
-	  <form onSubmit={submitHandler}>
-        <label htmlFor="color">Color:
-          <input type="text" name="color" id="color" />
-        </label>
-        <label htmlFor="size">Size in pixels:
-          <input type="number" min="50" max="500" name="size" defaultValue={120} />
-        </label>
-        <button>MAKE BOCKS</button>
-      </form>
-	)
+    return (
+        <form onSubmit={submitHandler}>
+            <label htmlFor="color">Color:
+                <input type="text" name="color" id="color" />
+            </label>
+            <label htmlFor="size">Size in pixels:
+                <input type="number" min="50" max="500" name="size" defaultValue={120} />
+            </label>
+            <button>MAKE BOCKS</button>
+        </form>
+    )
 };
 
 export default BoxForm;
