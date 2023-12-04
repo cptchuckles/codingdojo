18c47d353d1d165069978009380c47944d074dd5
Grendel <jcg@grendel.world>
2023-12-04 16:11:04 -0600

    algos(w9d1): solve part 1

--- algorithms/week09/day01.js
+++ algorithms/week09/day01.js
@@ -28,9 +28,64 @@ var arrB = [1, 3, 3, 5, 8, 10,];
  * @returns {number[]}
  */
 function mergeDedupe(arr1, arr2) {
-    //! write the PSEUDOCODE first!
+    const result = [];
+    for (let i=0, j=0; i < arr1.length || j < arr2.length;) {
+        const iNum = i < arr1.length ? arr1[i] : Infinity;
+        const jNum = j < arr2.length ? arr2[j] : Infinity;
+        if (iNum < jNum) {
+            i++;
+        }
+        else {
+            j++;
+        }
+        const num = Math.min(iNum, jNum);
+        if (result.length === 0 || result[result.length - 1] < num) {
+            result.push(num);
+        }
+    }
+    return result;
 }
 
 // try out some other tests
-mergeDedupe([1, 3, 3, 5, 8, 10], [1, 3, 4, 5]); // [ 1, 3, 4, 5, 8, 10 ]
-mergeDedupe([2, 3, 3, 5, 8, 10, 12], [1, 3, 4, 6]); // [1, 2, 3, 4, 5, 6, 8, 10, 12]
+console.log(mergeDedupe([1, 3, 3, 5, 8, 10], [1, 3, 4, 5])); // [ 1, 3, 4, 5, 8, 10 ]
+console.log(mergeDedupe([2, 3, 3, 5, 8, 10, 12], [1, 3, 4, 6])); // [1, 2, 3, 4, 5, 6, 8, 10, 12]
+
+
+/* 
+  Efficiently combine two already sorted multiset arrays 
+  into an array containing the sorted set intersection of the two.
+  Output: only the shared values between the two arrays (deduped).
+  Venn Diagram Visualization (bottom) https://i.ytimg.com/vi/sdflTUW6gHo/maxresdefault.jpg
+*/
+//! do not use Set()
+
+const arrA1 = [0, 1, 2, 2, 2, 7];
+const arrB1 = [2, 2, 6, 6, 7];
+const expected1 = [2, 7];
+
+const arrA2 = [0, 1, 2, 2, 2, 7];
+const arrB2 = [2, 2];
+const expected2 = [2];
+
+const arrA3 = [0, 1, 2, 2, 2, 7];
+const arrB3 = [10];
+const expected3 = [];
+
+/**
+ * Venn Diagram Visualization (bottom):
+ * https://i.ytimg.com/vi/sdflTUW6gHo/maxresdefault.jpg
+ * - Time: O(n) linear, n = max(sortedA.length, sortedB.length) when there are
+ *    dupes we may end up looping over all items of longer arr.
+ * - Space: O(n) linear, n = shorter array length.
+ * @param {Array<number>} sortedA
+ * @param {Array<number>} sortedB Both sets are multisets
+ *    (multi in that it can contain multiple dupes).
+ * @returns {Array<number>} returns The sorted set intersection: a new array that is
+ *    sorted and contains only the shared values between the two arrays
+ *    deduped.
+ */
+function orderedIntersection(sortedA, sortedB) {
+  //! write the PSEUDOCODE first!
+}
+
+orderedIntersection(arrA1, arrB1);
