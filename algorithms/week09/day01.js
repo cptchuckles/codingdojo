f412401ba275f1366c4612745ed80a5639ebd0c4
Grendel <jcg@grendel.world>
2023-12-04 10:11:44 -0600

    algos(w9d1): add problem

---
+++ algorithms/week09/day01.js
@@ -0,0 +1,36 @@
+// given two sorted arrays that may have duplicate values, merge them and remove any duplicates
+
+var arr1 = [1, 3, 3, 5, 8, 10,];
+var arr2 = [1, 3, 3, 5, 8, 10, 10, 10];
+// [1, 3, 5, 8, 10]
+
+// RIOT
+
+var arrA = [1, 3, 4, 5];
+var arrB = [1, 3, 3, 5, 8, 10,];
+// [1, 3, 4, 5, 8, 10]
+
+[]
+[1, 2, 2];
+
+// [1,2]
+
+// 1. DRIVER 🚗
+// 2. PRESENTER 🧑‍🏫
+// 3. NAVIGATOR 🧭
+
+
+/**
+ * Merge sorted arrays without duplicates
+ *
+ * @param {number[]} arr1
+ * @param {number[]} arr2
+ * @returns {number[]}
+ */
+function mergeDedupe(arr1, arr2) {
+    //! write the PSEUDOCODE first!
+}
+
+// try out some other tests
+mergeDedupe([1, 3, 3, 5, 8, 10], [1, 3, 4, 5]); // [ 1, 3, 4, 5, 8, 10 ]
+mergeDedupe([2, 3, 3, 5, 8, 10, 12], [1, 3, 4, 6]); // [1, 2, 3, 4, 5, 6, 8, 10, 12]
