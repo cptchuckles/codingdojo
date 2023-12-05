3c4b9d42ea39ce653b6909b4908f0df56157cea8
Grendel <jcg@grendel.world>
2023-12-05 10:23:54 -0600

    algos(w9d2): add solution

---
+++ algorithms/week09/day02.js
@@ -0,0 +1,39 @@
+// This is an actual interview algorithm given to a Coding Dojo alum
+
+// Find Consecutive Sums
+
+// You are given arr, a list of positive integers 0-255
+// You are given k, a integer between 1-255
+
+// Find all the consecutive groups of integers that add up to the value k
+
+// inputs
+k = 16
+arr = [2, 5, 3, 6, 7, 0, 0, 23, 11]
+
+// outputs
+// [
+//   [2, 5, 3, 6],
+//   [3, 6, 7]  // 3, 6, 7 appear consecutively, so they are including in the solution
+//   [3, 6, 7, 0],
+//   [3, 6, 7, 0, 0]
+// ]
+
+// create new arrays
+// if no matches, return empty array
+
+function findConsqSums(arr, k) {
+    const result = [];
+    for (let i=0; i<arr.length; i++) {
+        let sum = 0;
+        for (let j=i; j<arr.length; j++) {
+            sum += arr[j];
+            if (sum === k) {
+                result.push(arr.slice(i, j+1));
+            }
+        }
+    }
+    return result;
+}
+
+console.log(findConsqSums(arr, k));
