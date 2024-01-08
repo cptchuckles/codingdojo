d53ffdbdd2c29ff39ed5f4e46c81f57cd48da9b9
Grendel <jcg@grendel.world>
2024-01-08 14:00:11 -0600

    java: first assignment

---
+++ java/fundamentals/First.java
@@ -0,0 +1,7 @@
+public class First {
+    public static void main(String[] args) {
+        System.out.printf("Name: %s\n", "Jonny G McGee");
+        System.out.printf("Age: %d\n", 1337);
+        System.out.printf("Hometown: %s\n", "P. Sherman, 42 Wallaby Way, Sydney");
+    }
+}
