9e304da2d5139f30a8adfc5ef7e51d98a34fe7fd
Grendel <jcg@grendel.world>
2024-01-17 20:37:15 -0600

    java(books): complete Rendering Books assignment

--- java/spring/Books/src/main/java/world/grendel/books/controllers/BooksController.java
+++ java/spring/Books/src/main/java/world/grendel/books/controllers/BooksController.java
@@ -1,8 +1,13 @@
 package world.grendel.books.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import world.grendel.books.models.Book;
+import world.grendel.books.services.BookService;
 
 /**
  * BooksController
@@ -10,8 +15,23 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/books")
 public class BooksController {
+    private final BookService bookService;
+
+    public BooksController(BookService bookService) {
+        this.bookService = bookService;
+    }
+
     @GetMapping("")
     public String index() {
         return "booksIndex.jsp";
+    }
+
+    @GetMapping("/{id}")
+    public String show(@PathVariable("id") Long id,
+            Model model
+    ) {
+        Book shownBook = bookService.findBook(id);
+        model.addAttribute("shownBook", shownBook);
+        return "bookShow.jsp";
     }
 }
---
+++ java/spring/Books/src/main/webapp/WEB-INF/bookShow.jsp
@@ -0,0 +1,19 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8" import="java.util.Date"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@ taglib prefix="fmt" uri="http://java.sun.com/jsp/jstl/fmt" %>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8" />
+        <link rel="stylesheet" href="/webjars/bootstrap/css/bootstrap.min.css" />
+        <title>Book #${shownBook.id}</title>
+    </head>
+    <body>
+        <div class="container mt-5">
+            <h1><c:out value="${shownBook.title}" /></h1>
+            <p>Description: <c:out value="${shownBook.description}" /></p>
+            <p>Page Count: <c:out value="${shownBook.pageCount}" /></p>
+        </div>
+    </body>
+</html>
