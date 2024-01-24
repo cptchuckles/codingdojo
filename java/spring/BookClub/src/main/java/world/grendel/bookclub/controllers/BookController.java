770348afc2c4061b9d457666430c53d0a7490072
Grendel <jcg@grendel.world>
2024-01-24 17:29:38 -0600

    java(bookClub): add book edit

--- java/spring/BookClub/src/main/java/world/grendel/bookclub/controllers/BookController.java
+++ java/spring/BookClub/src/main/java/world/grendel/bookclub/controllers/BookController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import jakarta.servlet.http.HttpSession;
@@ -116,6 +117,55 @@ public class BookController {
             return "redirect:/books";
         }
         bookService.deleteById(id);
+        return "redirect:/books";
+    }
+
+    @GetMapping("/{id}/edit")
+    public String edit(
+        HttpSession session, Model model,
+        @PathVariable("id") Long id
+    ) {
+        try {
+            userService.getCurrentUser(session, model);
+        }
+        catch (Exception e) {
+            return "redirect:/logout";
+        }
+        Book book = bookService.getById(id);
+        User currentUser = (User) model.getAttribute("currentUser");
+        if (book.getUser().getId() != currentUser.getId()) {
+            return "redirect:/books";
+        }
+        model.addAttribute("book", book);
+        return "bookEdit.jsp";
+    }
+
+    @PutMapping("/{id}")
+    public String update(
+        HttpSession session, Model model,
+        @Valid @ModelAttribute("book") Book editedBook, BindingResult result
+    ) {
+        try {
+            userService.getCurrentUser(session, model);
+        }
+        catch (Exception e) {
+            return "redirect:/logout";
+        }
+
+        User currentUser = (User) model.getAttribute("currentUser");
+        Book originalBook = bookService.getById(editedBook.getId());
+        if (originalBook.getUser().getId() != currentUser.getId()) {
+            return "redirect:/books";
+        }
+        editedBook.setUser(originalBook.getUser());
+
+        if (result.hasErrors()) {
+            model.addAttribute("book", editedBook);
+            model.addAttribute("currentUser", currentUser);
+            return "bookEdit.jsp";
+        }
+
+        bookService.update(editedBook);
         return "redirect:/books";
     }
 }
