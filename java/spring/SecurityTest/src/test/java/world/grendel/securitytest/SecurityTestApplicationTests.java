5d69a9304c6d6e0f4dbcab1c207dc11b8bc2eba4
Grendel <jcg@grendel.world>
2024-01-29 17:42:57 -0600

    java(security): stub security test project

---
+++ java/spring/SecurityTest/src/test/java/world/grendel/securitytest/SecurityTestApplicationTests.java
@@ -0,0 +1,13 @@
+package world.grendel.securitytest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
