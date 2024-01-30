603225f5cc69ccece3d1ce6569695a8d90496703
Grendel <jcg@grendel.world>
2024-01-30 12:35:57 -0600

    java(auth): make the annotation work!!!!

--- java/spring/UserLoginDemo/src/main/java/world/grendel/userlogindemo/aspects/AuthenticatedRouteHandler.java
+++ java/spring/UserLoginDemo/src/main/java/world/grendel/userlogindemo/aspects/AuthenticatedRouteHandler.java
@@ -3,6 +3,7 @@ package world.grendel.userlogindemo.aspects;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.ui.Model;
@@ -21,10 +22,11 @@ public class AuthenticatedRouteHandler {
     @Autowired
     private UserService userService;
 
-    @Around(value = "@annotation(world.grendel.userlogindemo.annotation.AuthenticatedRoute)")
+    private static AuthenticatedRouteHandler instance = new AuthenticatedRouteHandler();
+
+	@Around(value = "@annotation(world.grendel.userlogindemo.annotation.AuthenticatedRoute)")
     public String authenticateRoute(
-        ProceedingJoinPoint joinPoint,
-        AuthenticatedRoute authenticatedRoute
+        ProceedingJoinPoint joinPoint
     ) throws Throwable {
         HttpSession session = null;
         Model model = null;
@@ -46,6 +48,8 @@ public class AuthenticatedRouteHandler {
             throw new Exception("Method does not use Model");
         }
 
+        AuthenticatedRoute authenticatedRoute = ((MethodSignature) joinPoint.getSignature()).getMethod().getAnnotation(AuthenticatedRoute.class);
+
         Long currentUserId = (Long) session.getAttribute("userId");
         if (currentUserId == null) {
             return authenticatedRoute.redirectPath();
@@ -58,6 +62,10 @@ public class AuthenticatedRouteHandler {
         model.addAttribute("currentUser", currentUser);
 
         return (String) joinPoint.proceed();
+    }
+
+    public static AuthenticatedRouteHandler aspectOf() {
+        return instance;
     }
 
     public UserService getUserService() {
